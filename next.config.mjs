3ab62c66b55a5586dab7f4b920a80256762df44d
Rick Imai <rick.imai@gmail.com>
2026-01-26 18:06:44 -0800

    Add CSP (report-only)

--- next.config.mjs
+++ next.config.mjs
@@ -6,23 +6,38 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains; preload',
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key: 'X-Frame-Options',
-            value: 'DENY',
+            key: "X-Frame-Options",
+            value: "DENY",
           },
           {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Content-Security-Policy-Report-Only",
+            value:
+              "default-src 'self'; " +
+              "base-uri 'self'; " +
+              "object-src 'none'; " +
+              "frame-ancestors 'none'; " +
+              "form-action 'self'; " +
+              "img-src 'self' data: https:; " +
+              "font-src 'self' data: https:; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+              "connect-src 'self' https:; " +
+              "upgrade-insecure-requests",
           },
         ],
       },
@@ -31,3 +46,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
